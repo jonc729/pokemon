250a997c6961e18b2a483a0bddc9da00be603e94
jonc729 <106701244+jonc729@users.noreply.github.com>
2022-10-10 21:00:35 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const dragonite = {
+    type1: "dragon",
+    type2: "flying",
+    hp: 386,
+    attack: 403,
+    defense: 317,
+    spAttack: 254,
+    spDefense: 254, 
+    speed: 222,
+}
+
+const gengar = {
+    type1: "ghost",
+    type2: "poison",
+    hp: 324,
+    attack: 251,
+    defense: 240,
+    spAttack: 394,
+    spDefense: 273, 
+    speed: 350,
+}
+
+let player1hp = dragonite.hp
+let player2hp = gengar.hp
+
+console.log(player1hp)
+console.log(player2hp)
